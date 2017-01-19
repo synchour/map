c861d9b61b2221c53d0ec69a8a298a9ea5c9ce6d
Hao Shen <sh@cloudcutout.com>
2017-01-19 20:05:00 +0100

    api documentation with swagger

--- map/src/main/java/com/sh/map/repository/SFMovieRepository.java
+++ map/src/main/java/com/sh/map/repository/SFMovieRepository.java
@@ -9,10 +9,8 @@ import org.springframework.data.repository.CrudRepository;
 import com.sh.map.data.SFMovie;
 
 public interface SFMovieRepository extends CrudRepository<SFMovie, Long> {
-
-	List<SFMovie> findByTitleContainingIgnoreCase(String title);
-	
 	List<SFMovie> findByTitle(String title);
 	
+	// This is not used in SFMovie, however 2d spatial search can be done this way easily
 	List<SFMovie> findByGeoLocationNear(Point location, Distance distance);
 }
--- map/src/main/java/com/sh/map/restful/SFMovieService.java
+++ map/src/main/java/com/sh/map/restful/SFMovieService.java
@@ -3,7 +3,10 @@ package com.sh.map.restful;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sh.map.data.SFMoviesResponse;
@@ -11,6 +14,8 @@ import com.sh.map.data.SFMoviesTitleResponse;
 import com.sh.map.repository.MovieServiceDataAccess;
 import com.sh.map.repository.SFMovieRepository;
 import com.sh.map.repository.SFMovieTitleRepository;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/sfmovie")
@@ -28,8 +33,11 @@ public class SFMovieService{
 	private static Logger logger = LoggerFactory.getLogger(SFMovieService.class);
 	
 	//This is used for UI lookup (containing match)
-	@RequestMapping("/titles") 
-	public SFMoviesTitleResponse findTitles(String searchTerm) {
+	@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "/titles",
+		    method = RequestMethod.GET) 
+	@ApiOperation(value = "Find list of movie title containing search term",
+    	response = SFMoviesTitleResponse.class)
+	public SFMoviesTitleResponse findTitles(@RequestParam("searchTerm") String searchTerm) {
 		logger.info("/titles " + searchTerm);
 		if (searchTerm == null || searchTerm.isEmpty()) {
 			return new SFMoviesTitleResponse();
@@ -38,8 +46,12 @@ public class SFMovieService{
 	}
 	
 	//This is used for finding all location from an title (exact match)
-	@RequestMapping("/titleLocations") 
-	public SFMoviesResponse findTitleLocations(String title) {
+	
+	@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "/titleLocations",
+			method = RequestMethod.GET)
+	@ApiOperation(value = "Find list of movie locations matching the exact tile",
+		response = SFMoviesResponse.class)
+	public SFMoviesResponse findTitleLocations(@RequestParam("title") String title) {
 		logger.info("/titleLocations " + title);
 		if (title == null || title.isEmpty()) {
 			return new SFMoviesResponse();
